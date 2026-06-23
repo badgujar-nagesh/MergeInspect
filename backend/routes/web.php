de135c836c2bbf73041946339137bc2949d74460
Nagesh Badgujar <nageshbadgujar@gmail.com>
2026-06-23 13:06:43 +0530

    add secured artisan commands

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,59 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+function authorizeDeploymentCommand()
+{
+    $provided = request()->header('X-Deploy-Secret');
+
+    abort_unless(
+        hash_equals(env('DEPLOY_SECRET'), $provided ?? ''),
+        403,
+        'Unauthorized'
+    );
+}
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/system/migrate', function () {
+
+    authorizeDeploymentCommand();
+
+    Artisan::call('migrate', [
+        '--force' => true
+    ]);
+
+    return response()->json([
+        'success' => true,
+        'output' => Artisan::output(),
+    ]);
+});
+
+Route::get('/system/seed', function () {
+
+    authorizeDeploymentCommand();
+
+    Artisan::call('db:seed', [
+        '--force' => true
+    ]);
+
+    return response()->json([
+        'success' => true,
+        'output' => Artisan::output(),
+    ]);
+});
+
+Route::get('/system/cache-clear', function () {
+
+    authorizeDeploymentCommand();
+
+    Artisan::call('optimize:clear');
+
+    return response()->json([
+        'success' => true,
+        'output' => Artisan::output(),
+    ]);
 });
